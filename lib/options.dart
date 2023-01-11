6ed498ce2071f161ead0b19fb262020519e5ebc2
andre-alck <andrealck1@gmail.com>
2023-01-10 23:04:46 -0300

    feat: initialise tech_stack and options widget

---
+++ lib/options.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Options extends StatelessWidget {
+  const Options({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(children: [
+        Container(
+            decoration: const BoxDecoration(
+                border: Border(
+                    right: BorderSide(width: 2, color: Colors.black),
+                    bottom: BorderSide(width: 2, color: Colors.black))),
+            padding: const EdgeInsets.all(20),
+            child: const Text('sobre')),
+        Container(
+            decoration: const BoxDecoration(
+                border:
+                    Border(bottom: BorderSide(width: 2, color: Colors.black))),
+            padding: const EdgeInsets.all(20),
+            child: const Text('porfólio'))
+      ]),
+      Row(children: [
+        Container(
+            decoration: const BoxDecoration(
+                border:
+                    Border(right: BorderSide(width: 2, color: Colors.black))),
+            padding: const EdgeInsets.all(20),
+            child: const Text('resumo')),
+        Container(
+            padding: const EdgeInsets.all(20), child: const Text('contato'))
+      ])
+    ]);
+  }
+}
