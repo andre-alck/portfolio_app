2cf5f28e29de8c7c8006aca86977e4a51e946109
andre-alck <andrealck1@gmail.com>
2023-01-12 00:22:12 -0300

    fix: center options widget

--- lib/widgets/options_widget.dart
+++ lib/widgets/options_widget.dart
@@ -5,68 +5,65 @@ class OptionsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(children: [
-            Container(
-                width: 100,
-                height: 100,
-                decoration: const BoxDecoration(
-                    border: Border(
-                        right: BorderSide(width: 2, color: Colors.white),
-                        bottom: BorderSide(width: 2, color: Colors.white))),
-                child: Center(
-                    child: RichText(
-                        text: const TextSpan(
-                            text: 'sobre',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontFamily: "Regular"))))),
-            Container(
-                width: 100,
-                height: 100,
-                decoration: const BoxDecoration(
-                    border: Border(
-                        bottom: BorderSide(width: 2, color: Colors.white))),
-                child: Center(
-                    child: RichText(
-                        text: const TextSpan(
-                            text: 'portfólio',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontFamily: "Regular")))))
-          ]),
-          Row(children: [
-            Container(
-                width: 100,
-                height: 100,
-                decoration: const BoxDecoration(
-                    border: Border(
-                        right: BorderSide(width: 2, color: Colors.white))),
-                child: Center(
-                    child: RichText(
-                        text: const TextSpan(
-                            text: 'portfólio',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontFamily: "Regular"))))),
-            SizedBox(
-                width: 100,
-                height: 100,
-                child: Center(
-                    child: RichText(
-                        text: const TextSpan(
-                            text: 'portfólio',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontFamily: "Regular")))))
-          ])
-        ]);
+    return Column(children: [
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Container(
+            width: 100,
+            height: 100,
+            decoration: const BoxDecoration(
+                border: Border(
+                    right: BorderSide(width: 1, color: Colors.white),
+                    bottom: BorderSide(width: 1, color: Colors.white))),
+            child: Center(
+                child: RichText(
+                    text: const TextSpan(
+                        text: 'sobre',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontFamily: "Regular"))))),
+        Container(
+            width: 100,
+            height: 100,
+            decoration: const BoxDecoration(
+                border:
+                    Border(bottom: BorderSide(width: 1, color: Colors.white))),
+            child: Center(
+                child: RichText(
+                    text: const TextSpan(
+                        text: 'portfólio',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontFamily: "Regular")))))
+      ]),
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Container(
+            width: 100,
+            height: 100,
+            decoration: const BoxDecoration(
+                border:
+                    Border(right: BorderSide(width: 1, color: Colors.white))),
+            child: Center(
+                child: RichText(
+                    text: const TextSpan(
+                        text: 'resumo',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontFamily: "Regular"))))),
+        SizedBox(
+            width: 100,
+            height: 100,
+            child: Center(
+                child: RichText(
+                    text: const TextSpan(
+                        text: 'contato',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontFamily: "Regular")))))
+      ])
+    ]);
   }
 }
